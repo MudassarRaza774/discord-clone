c1482d05339b6945a29b38a34dbabc92de7bf99e
Mudassar Raza <mudasarraza049@gmail.com>
2023-09-01 03:36:24 +0500

    Server join using link & Edit server settings
    Now you can let the other members join the your server using server link as a guest. Moreover you can now edit server name and image

---
+++ app/api/server/[serverId]/route.ts
@@ -0,0 +1,35 @@
+import { currentProfile } from "@/lib/currentProfile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+
+    const { name, imageUrl } = await req.json();
+
+    const { serverId } = params;
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        profileId: profile.id,
+      },
+      data: {
+        name,
+        imageUrl,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[ServerId_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
