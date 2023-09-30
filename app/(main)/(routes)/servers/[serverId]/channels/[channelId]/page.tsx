4eaace5beee92afbc73e3e073603c00b37e7966b
Mudassar Raza <mudasarraza049@gmail.com>
2023-10-01 02:57:26 +0500

    Socket and Ui setup for conversations

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import { redirectToSignIn } from "@clerk/nextjs";
 import ChatHeader from "@/components/chat/ChatHeader";
+import { ChatInput } from "@/components/chat/ChatInput";
 import { db } from "@/lib/db";
 import { currentProfile } from "@/lib/currentProfile";
 
@@ -41,6 +42,16 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
+      />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
       />
     </div>
   );
